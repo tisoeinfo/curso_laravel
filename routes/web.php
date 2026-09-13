078118bb2f4612d3f5e7fa3c547fd1e5f75cd373
saulbarretominaya <saulbarretominaya@gmail.com>
2026-09-12 20:44:56 -0500

    Subiendo Proyecto Laravel

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/saludo', function () {
+    return 'Hola desde Laravel';
+});
+
+Route::get('/api/prueba', function () {
+    return response()->json([
+        'mensaje' => 'Hola desde Laravel API',
+        'version' => '1.0'
+    ]);
+});
+
+Route::get('/api/saludo/{nombre}', function ($nombre) {
+    return response()->json([
+        'mensaje' => 'Hola ' . $nombre
+    ]);
+});
+
+Route::get('/api/saludo/{nombre}/{edad}', function ($nombre, $edad) {
+    return response()->json([
+        'mensaje' => 'Hola ' . $nombre,
+        'edad' => $edad
+    ]);
+});
